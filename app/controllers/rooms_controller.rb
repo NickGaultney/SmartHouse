67b0e1f62fb574e3fcef7c3f21f2506a18da61ff
Nick Mark <nmgaultney@gmail.com>
2021-03-26 15:00:12 -0700

    room actions

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,63 @@
+class RoomsController < ApplicationController
+
+	def garage
+		redirect
+	end
+
+	def laundry
+		redirect
+	end
+
+	def kitchen
+		redirect
+	end
+
+	def dining
+		redirect
+	end
+
+	def living
+		redirect
+	end
+
+	def pavilion
+		redirect
+	end
+
+	def hallway
+		redirect
+	end
+
+	def guest_1
+		redirect
+	end
+
+	def guest_2
+		redirect
+	end
+
+	def guest_bathroom
+		redirect
+	end
+
+	def master_bathroom
+		redirect
+	end
+
+	def master_bedroom
+		redirect
+	end
+
+	def master_closet
+		redirect
+	end
+
+	def front_patio
+		redirect
+	end
+
+	private
+	def redirect
+		head :no_content
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   root 'welcome#welcome'
+
+  get 'garage', to: 'rooms#garage'
+  get 'laundry', to: 'rooms#laundry'
+  get 'kitchen', to: 'rooms#kitchen'
+  get 'dining', to: 'rooms#dining'
+  get 'living', to: 'rooms#living'
+  get 'pavilion', to: 'rooms#pavilion'
+  get 'hallway', to: 'rooms#hallway'
+  get 'guest_1', to: 'rooms#guest_1'
+  get 'guest_2', to: 'rooms#guest_2'
+  get 'guest_bathroom', to: 'rooms#guest_bathroom'
+  get 'master_bathroom', to: 'rooms#master_bathroom'
+  get 'master_bedroom', to: 'rooms#master_bedroom'
+  get 'master_closet', to: 'rooms#master_closet'
+  get 'front_patio', to: 'rooms#front_patio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
