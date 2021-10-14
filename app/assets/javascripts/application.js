bb496b356563bac91177d573bb1191e9aadab71c
Nick Mark <nmgaultney@gmail.com>
2021-10-14 16:08:14 -0600

    working_draggable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,54 +20,68 @@
 $(document).on('turbolinks:load', function() {
 	$('img[usemap]').rwdImageMaps();
 
-	$( ".bulb" ).draggable({
-	    revert: 'invalid',
-	    drag: function(event, ui) {
-
-	    }
-	});
-
 	$( ".outfit" ).droppable({
 		tolerance: "pointer",
 	    accept: ".bulb",
 	    drop: function(event, ui) {
-	        if (!confirm("Are you sure?")) {
-	            ui.draggable.css({
-	            top: 0,
-	            left: 0
-	        	}); 
-	    	} else {
-	    		var left = (ui.draggable.css("left").slice(0, -2) / ($(".outfit").width() / 100)) + "%";
-	    		var top = (ui.draggable.css("top").slice(0, -2) / ($(".outfit").height() / 100)) + "%";
-	    		ui.draggable.css("left", left);
-	    		ui.draggable.css("top", top);
+    		var left = (ui.draggable.css("left").slice(0, -2) / ($(".outfit").width() / 100)) + "%";
+    		var top = (ui.draggable.css("top").slice(0, -2) / ($(".outfit").height() / 100)) + "%";
+    		ui.draggable.css("left", left);
+    		ui.draggable.css("top", top);
 
-	        	$.ajax({
-				  url: "/buttons/" + ui.draggable.attr("data-id"),
-				  type: "put",
-				  data: {
-				  	authenticity_token: $('[name="csrf-token"]')[0].content,
-				  	button: {
-				  		coordinates: left + "," + top
-				  	}
-				  },
-				  success: function(data) {},
-				  error: function(data) {}
-				})
-	        };
+        	$.ajax({
+			  url: "/buttons/" + ui.draggable.attr("data-id"),
+			  type: "put",
+			  data: {
+			  	authenticity_token: $('[name="csrf-token"]')[0].content,
+			  	button: {
+			  		coordinates: left + "," + top
+			  	}
+			  },
+			  success: function(data) {},
+			  error: function(data) {}
+			})
 	    }
 	});
 })
 
+function getEditButton() {
+	return document.getElementById("edit-mode");
+}
 
-var wait;
-window.onresize = function() {
-	clearTimeout(wait);
-	wait = setTimeout(function() {
-		$( ".bulb" ).each(function( index ) {
-			//$(this).css("left", parseInt($(this).attr('coords').split(",")[0]) / ($(".outfit").width() / 100) + "%");
-			//$(this).css("top", parseInt($(this).attr('coords').split(",")[1]) / ($(".outfit").height() / 100) + "%");
-			//$(this).attr("coords", $("#" + $(this).attr("class").split(/\s+/)[1]).attr('coords'));
-		});
-    }, 100);
-};
+function toggleEditMode() {
+	var button = getEditButton();
+
+	if (button.getAttribute("state") === "false") {
+		button.setAttribute("state", "true");
+		enableDraggable(".bulb");
+	} else {
+		button.setAttribute("state", "false");
+		disableDraggable(".bulb");
+	}
+}
+
+function enableDraggable(e) {
+	$(e).draggable({
+		containment: '.outfit',
+	    revert: 'invalid',
+	    drag: function(event, ui) {
+	    }
+	});
+
+	$(e).draggable('enable');
+
+	var button = getEditButton();
+	button.classList.remove('btn-success');
+	button.classList.add('btn-warning');
+	button.innerHTML = "Disable Edit Mode";
+}
+
+function disableDraggable(e) {
+	$(e).draggable('disable');
+
+	var button = getEditButton();
+	button.classList.remove('btn-warning');
+	button.classList.add('btn-success');
+	button.innerHTML = "Enable Edit Mode";
+}
