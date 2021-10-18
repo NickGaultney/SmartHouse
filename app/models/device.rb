aa20630e584f8b413acf37a1ebd4f9d553906f8c
NickGaultney <nmgaultney@gmail.com>
2021-10-17 22:09:06 -0500

    configure_tasmota_via_device

--- app/models/device.rb
+++ app/models/device.rb
@@ -8,6 +8,8 @@ class Device < ApplicationRecord
 		mqtt_user = "homeiot"
 		mqtt_password = "12345678"
 		topic = self.id.to_s + "_" + self.name.to_s
-		HTTP.get("http://#{ip_address}/cm?cmnd=backlog%20mqtthost%20#{mqtt_host}%3Bmqttuser%20#{mqtt_user}%3Bmqttpassword%20#{mqtt_password}%3Btopic%20#{topic}%3B")
+		template = "{%22NAME%22:%22Sonoff%20MINIR2%22,%22GPIO%22:[17,0,0,0,9,0,0,0,21,56,0,0,0],%22FLAG%22:0,%22BASE%22:1}"
+
+		HTTP.get("http://#{ip_address}/cm?cmnd=backlog%20mqtthost%20#{mqtt_host}%3Bmqttuser%20#{mqtt_user}%3Bmqttpassword%20#{mqtt_password}%3Btopic%20#{topic}%3Btemplate%20#{template}%3B")
 	end
 end
