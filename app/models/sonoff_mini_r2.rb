a5e167edd6d59dacf170c738483dbbdd0a7e3d1e
NickGaultney <nmgaultney@gmail.com>
2022-01-11 20:59:19 +0000

    change default io name

--- app/models/sonoff_mini_r2.rb
+++ app/models/sonoff_mini_r2.rb
@@ -1,6 +1,9 @@
 class SonoffMiniR2 < IoDevice
 	def generate_io
-		Input.create(name: "#{self.name}_SMR2_#{self.id}_in", input_type: "Switch", io_device: self)
-		Output.create(name: "#{self.name}_SMR2_#{self.id}_out", output_type: "Relay", io_device: self)
+		#Input.create(name: "#{self.name}_SMR2_#{self.id}_in", input_type: "Switch", io_device: self)
+		#Output.create(name: "#{self.name}_SMR2_#{self.id}_out", output_type: "Relay", io_device: self)
+
+		Input.create(name: "#{self.name}", input_type: "Switch", io_device: self)
+		Output.create(name: "#{self.name}", output_type: "Relay", io_device: self)
 	end
 end
